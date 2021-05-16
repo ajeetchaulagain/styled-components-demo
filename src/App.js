8929a5571c4ea93a39bbfb1f79e3290b029bf89b
ajeetchaulagain <chaulagainajeet@gmail.com>
2021-05-16 20:17:34 +1000

    add basic styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+// Create a Wrapper component that'll render a <section> tag with some styles
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+
 function App() {
   return (
-   <div>
-     <h2>Styled Components Demonstration</h2>
-   </div>
+    <Wrapper>
+    <Title>
+      Hello World!
+    </Title>
+  </Wrapper>
   );
 }
 
